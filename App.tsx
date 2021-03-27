c391ae0bf12790ba9c0329b7e512a50573d75bba
bufgix <ooruc471@yandex.com>
2021-03-28 01:49:18 +0300

    refactor: persist state

--- App.tsx
+++ App.tsx
@@ -3,11 +3,19 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStackScreen } from '@/navigation';
 import { ThemeProvider } from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persist } from 'mst-persist';
 
 import { StoreProvider, initStore } from '@/models';
 import Theme from '@/styles/theme';
 
 const store = initStore();
+
+persist('@State_user', store, {
+  storage: AsyncStorage,
+  jsonify: true,
+  whitelist: ['user'],
+});
 
 const App = () => {
   return (
